f059eb782054d0ac1175131bb17be88cce5069fc
Sujoy Biswas <96237257+sujoyonweb@users.noreply.github.com>
2026-02-22 12:26:39 +0530

    bug fix - font preload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 1. Change this version number every time you push an update to GitHub!
-const CACHE_NAME = 'blok-cache-v14.8';
+const CACHE_NAME = 'blok-cache-v14.7';
 
 // 2. The exact list of files needed for offline mode
 // Note: We use './' (relative paths) so it works perfectly on GitHub Pages
@@ -101,5 +101,4 @@ self.addEventListener('message', (event) => {
     if (event.data && event.data.type === 'SKIP_WAITING') {
         self.skipWaiting();
     }
-
 });
